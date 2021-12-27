d019510184625d940cb2eb48f8cdea54d6c8551a
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-12-27 12:11:54 -0600

    installed the react drag and drop package
    installed a package to assist with the drag and drop capabilities of this application, used the useDrag custom hook to make each todo item draggable so far

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 import { Header } from './components/Header/Header';
 import { TodoList } from './components/TodoList/TodoList';
 import React from 'react';
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <TodoList />
       <p>Drag and drop to reorder list</p>
-    </>
+    </DndProvider>
   );
 }
 

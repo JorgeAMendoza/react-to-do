a39820f278d8a73244f5de1da8d33ecb09955c42
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-01-07 06:35:25 -0600

    insert styled componets for header, create svg components for the sun and moon components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,25 @@
+import { StyledHeader } from '../../styles/Header/Header.styled';
+import { HeaderInfo } from '../../styles/Header/HeaderInfo.styled';
+import { Container } from '../../styles/Utils/Container.styled';
+import { Moon } from './ToggleIcons/Moon';
+import { Sun } from './ToggleIcons/Sun';
+
 interface headerProps {
   toggleTheme: () => void;
+  themeValue: string;
 }
 
-export const Header = ({ toggleTheme }: headerProps) => {
+export const Header = ({ toggleTheme, themeValue }: headerProps) => {
   return (
-    <header>
-      <h1>TODO</h1>
-      <button onClick={() => toggleTheme()}>Toggle Theme</button>
-    </header>
+    <StyledHeader>
+      <Container>
+        <HeaderInfo>
+          <h1>TODO</h1>
+          <button onClick={() => toggleTheme()}>
+            {themeValue === 'light' ? <Moon /> : <Sun />}
+          </button>
+        </HeaderInfo>
+      </Container>
+    </StyledHeader>
   );
 };

46882f094b4489b9676f01bcf8976e4b76436bd4
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-01-07 06:36:09 -0600

    add theme toggle to the aplication, create multiple styled components for the header

--- src/styles/Header/Header.styled.ts
+++ src/styles/Header/Header.styled.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
+  padding: 4rem 0;
   border: 1px solid black;
+
+  svg:hover {
+    fill: ${({ theme }) => theme.color.active};
+  }
 `;

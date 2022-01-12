cd4ef73a7e7ae4fb739d3c68a41f01ee8745175e
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-01-12 06:26:52 -0600

    items can be dragged on top of each other, using data set we can save the todo ID

--- src/components/TodoList/TodoDisplay/Todo.tsx
+++ src/components/TodoList/TodoDisplay/Todo.tsx
@@ -2,6 +2,7 @@ import { useDrag } from 'react-dnd';
 import { TodoProps } from '../../../types/TodoProps/todo-props';
 import { Cross } from './Cross';
 import { TodoStyled } from '../../../styles/TodoList/Todo/Todo.styled';
+import { DragEventHandler } from 'react';
 
 export const ToDo = ({
   status,
@@ -10,12 +11,27 @@ export const ToDo = ({
   deleteTodo,
   updateTodoStatus,
 }: TodoProps) => {
-  const [collected, drag, dragPreview] = useDrag(() => ({
-    type: 'TODO_DRAG',
-    item: { todoID },
-  }));
+  // const [collected, drag, dragPreview] = useDrag(() => ({
+  //   type: 'TODO_DRAG',
+  //   item: { todoID },
+  // }));
+
   return (
-    <TodoStyled shouldCross={status} ref={drag}>
+    <TodoStyled
+      shouldCross={status}
+      draggable="true"
+      onDragStart={(e) => {
+        console.log('start Drag');
+        e.dataTransfer.setData('todoID', todoID.toString());
+      }}
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        console.log('Reordering items');
+        console.log(todoID, Number(e.dataTransfer.getData('todoID')));
+      }}
+    >
       <label>
         <input
           type="checkbox"
